61aea78d4ecb4c274744363e0da065a9a6d325d4
Rene Brandel <renbran@amazon.com>
2024-05-16 17:51:24 -0400

    added more verbose postbuild script

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -5,3 +5,9 @@ cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r ./build/client ./.amplify-hosting/static
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+ls ./.amplify-hosting
+ls ./.amplify-hosting/compute/default
+ls ./.amplify-hosting/static
+ls ./.amplify-hosting/static/assets
+cat ./.amplify-hosting/deploy-manifest.json
